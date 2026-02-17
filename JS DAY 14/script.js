d662ab2ecf962eb642eb7ce0e159bd8923596dd7
aaditprashar <prasharaadit@gmail.com>
2026-02-17 15:46:30 +0530

    Day 14

--- JS DAY 14/script.js
+++ JS DAY 14/script.js
@@ -1,29 +1,31 @@
-// // Promise creation
-// let myPromise=new Promise((res,rej)=>{
-//     let data = "This is data";
-//     if(data){
-//         res(data);
-//     } else{
-//         rej("error");
-//     }
-// });
+// Promise creation
+let myPromise=new Promise((res,rej)=>{
+    let data = "This is data";
+    if(data){
+        res(data);
+    } else{
+        rej("error");
+    }
+});
 
-// // myPromise
-// //     .then((data)=>{
-// //     console.log(data);
-// // },(error)=>{
-// //     console.log(error);
-// // });
+myPromise
+    .then((data)=>{
+    console.log(data);
+},(error)=>{
+    console.log(error);
+});
 
-// myPromise
-//     .then((data)=>{
-//     console.log(data);
-//     })
-//     .catch((err)=>{
-//     console.log(err);
-//     });
+myPromise
+    .then((data)=>{
+    console.log(data);
+    })
+    .catch((err)=>{
+    console.log(err);
+    });
 
-// console.log(myPromise);
+console.log(myPromise);
+
+// ----------------------------------
 
 let resp=fetch('https://jsonplaceholder.typicode.com/todos/1');
 resp.then((robj)=>{
@@ -32,3 +34,10 @@ resp.then((robj)=>{
         console.log(data);
     });
 });
+
+// -------------------------------------
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then(robj=>robj.json())
+.then(data=>console.log(data))
+.then(err => console.log(err));
